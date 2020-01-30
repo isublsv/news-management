9aa04e4ccff2fc9035f1d56e6b78575aa70005fd
Dzmitry Gartsmanovich <Dzmitry_Gartsmanovich@epam.com>
2020-01-30 15:36:07 +0300

    Add Service configuration. Add ModelMapper bean congiruation. Add Transaction support.

---
+++ service/src/main/java/com/epam/lab/configuration/ServiceConfiguration.java
@@ -0,0 +1,38 @@
+package com.epam.lab.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("com.epam.lab")
+public class ServiceConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                //Tokens are matched in strict order
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                //unable field matching
+                .setFieldMatchingEnabled(true)
+                //null skipping
+                .setSkipNullEnabled(true)
+                //access to private fields
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
+    }
+
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager(HikariDataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
