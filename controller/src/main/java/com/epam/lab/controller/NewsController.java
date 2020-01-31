8ca498c17e549cc88613d276c4071b3ae28c4ed6
Dzmitry Gartsmanovich <Dzmitry_Gartsmanovich@epam.com>
2020-01-31 17:01:57 +0300

    Add AuthorDto info.

--- controller/src/main/java/com/epam/lab/controller/NewsController.java
+++ controller/src/main/java/com/epam/lab/controller/NewsController.java
@@ -1,9 +1,14 @@
 package com.epam.lab.controller;
 
+import com.epam.lab.dto.AuthorDto;
 import com.epam.lab.dto.NewsDto;
+import com.epam.lab.dto.TagDto;
 import com.epam.lab.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/")
@@ -23,6 +28,19 @@ public class NewsController {
         newsDto.setTitle("Big news!");
         newsDto.setShortText("BLABLA!");
         newsDto.setFullText("Full text!");
+
+        AuthorDto authorDto = new AuthorDto();
+        authorDto.setName("Name");
+        authorDto.setSurname("Surname");
+        newsDto.setAuthorDto(authorDto);
+
+        TagDto tagDto = new TagDto("Tag1");
+        TagDto tagDto1 = new TagDto("Tag2");
+        List<TagDto> tagDtos = new ArrayList<>();
+        tagDtos.add(tagDto);
+        tagDtos.add(tagDto1);
+
+        newsDto.setTagDtos(tagDtos);
         return newsDto;
     }
 
