4579f6d4339687dfdd44f0c6b533d8b94f7d33a2
Dmitry G <isublsv@gmail.com>
2020-02-02 22:21:48 +0300

    Add addTagsForNews method

--- service/src/main/java/com/epam/lab/service/NewsService.java
+++ service/src/main/java/com/epam/lab/service/NewsService.java
@@ -1,8 +1,13 @@
 package com.epam.lab.service;
 
 import com.epam.lab.dto.NewsDto;
+import com.epam.lab.dto.TagDto;
+
+import java.util.List;
 
 public interface NewsService extends Service<NewsDto> {
 
+    void addTagsForNews(NewsDto newsDto, List<TagDto> tagDtos);
+    
     Long countAllNews();
 }
