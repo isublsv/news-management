bbe4e78fea5e6f8edccab49a486d1199dc66a09f
Dmitry G <isublsv@gmail.com>
2020-02-02 22:22:19 +0300

    Add custom Exception

---
+++ service/src/main/java/com/epam/lab/exception/ServiceException.java
@@ -0,0 +1,18 @@
+package com.epam.lab.exception;
+
+public class ServiceException extends Exception {
+    public ServiceException() {
+    }
+
+    public ServiceException(final String message) {
+        super(message);
+    }
+
+    public ServiceException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(final Throwable cause) {
+        super(cause);
+    }
+}
