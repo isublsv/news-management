2ac81fbd0e1ffe7c2bc5e44713c425b0e1c78c0c
Dmitry G <isublsv@gmail.com>
2020-02-02 22:21:22 +0300

    Change create method signature

--- service/src/main/java/com/epam/lab/service/Service.java
+++ service/src/main/java/com/epam/lab/service/Service.java
@@ -1,10 +1,11 @@
 package com.epam.lab.service;
 
 import com.epam.lab.dto.AbstractDto;
+import com.epam.lab.exception.ServiceException;
 
 public interface Service<D extends AbstractDto> {
 
-    D create(D entityDto) throws Exception;
+    D create(D entityDto) throws ServiceException;
     D find(long id);
     void update(D entityDto);
     void delete(long id);
