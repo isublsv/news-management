def51d4d20b7bb06b58a54b26add32eaef04af0b
Dzmitry Gartsmanovich <Dzmitry_Gartsmanovich@epam.com>
2020-02-07 14:48:25 +0300

    View rework.

--- src/main/resources/news_db_schema_generation.sql
+++ src/main/resources/news_db_schema_generation.sql
@@ -83,13 +83,30 @@ CREATE INDEX idx_author_name_surname ON news.author (name) INCLUDE (surname);
 
 CREATE INDEX idx_news_title ON news.news (title);
 
-SELECT search_by.id, search_by.tag_ids, news.author.id
-FROM (SELECT news.news.id, array_agg(tag.id) AS tag_ids
-      FROM news.news
-               LEFT JOIN news.news_tag ON news.news.id = news.news_tag.news_id
-               LEFT JOIN news.tag ON news.tag.id = news.news_tag.tag_id GROUP BY news.news.id) search_by
-         LEFT JOIN news.news_author ON search_by.id = news.news_author.news_id
-         LEFT JOIN news.author ON news.news_author.author_id = news.author.id;
+ SELECT search_by.id AS news_id,
+    search_by.title,
+    search_by.short_text,
+    search_by.full_text,
+    search_by.creation_date AS date,
+    search_by.tag_ids,
+    search_by.tag_names,
+    author.id AS author_id,
+    author.name AS author_name,
+    author.surname AS author_surname
+   FROM ( SELECT news.id,
+            news.title,
+            news.short_text,
+            news.full_text,
+            news.creation_date,
+            news.modification_date,
+            array_agg(tag.id) AS tag_ids,
+            array_agg(tag.name) AS tag_names
+           FROM news.news
+             LEFT JOIN news.news_tag ON news.id = news_tag.news_id
+             LEFT JOIN news.tag ON tag.id = news_tag.tag_id
+          GROUP BY news.id) search_by
+     LEFT JOIN news.news_author ON search_by.id = news_author.news_id
+     LEFT JOIN news.author ON news_author.author_id = author.id;
 
 ALTER TABLE news.author
     OWNER to postgres;
