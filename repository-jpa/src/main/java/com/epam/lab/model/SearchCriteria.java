7a615b8df638947a7eb079513d1012ccd5d0ee3f
Dzmitry Gartsmanovich <Dzmitry_Gartsmanovich@epam.com>
2020-02-28 16:06:01 +0300

    Remove redundant validation

--- repository-jpa/src/main/java/com/epam/lab/model/SearchCriteria.java
+++ repository-jpa/src/main/java/com/epam/lab/model/SearchCriteria.java
@@ -1,31 +1,15 @@
 package com.epam.lab.model;
 
-import javax.validation.constraints.Pattern;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class SearchCriteria extends AbstractEntity{
+public class SearchCriteria extends AbstractEntity {
 
-    @Pattern(regexp = "[A-ZА-Я]{2,30}",
-            flags = Pattern.Flag.CASE_INSENSITIVE,
-            message = "Provided name is not valid")
     private String name;
-
-    @Pattern(regexp = "[A-ZА-Я\\-]{2,30}",
-            flags = Pattern.Flag.CASE_INSENSITIVE,
-            message = "Provided surname is not valid")
     private String surname;
-
-    private Set<@Pattern(regexp = "[A-ZА-Я\\-\\d ]{2,30}",
-            flags = Pattern.Flag.CASE_INSENSITIVE,
-            message = "Provided tag name is not valid") String> tags = new HashSet<>();
-
-    private Set<@Pattern(regexp = "[A-ZА-Я\\-\\d ]+",
-            flags = Pattern.Flag.CASE_INSENSITIVE,
-            message = "Provided column name is not valid") String> orderBy = new LinkedHashSet<>();
-    
-    private boolean desc;
+    private Set<String> tags = new HashSet<>();
+    private Set<String> orderBy = new LinkedHashSet<>();
 
     public String getName() {
         return name;
@@ -59,17 +43,9 @@ public class SearchCriteria extends AbstractEntity{
         orderBy = orderByValue;
     }
 
-    public boolean isDesc() {
-        return desc;
-    }
-
-    public void setDesc(final boolean descValue) {
-        desc = descValue;
-    }
-
     @Override
     public String toString() {
-        return String.format("SearchCriteria{name='%s', surname='%s', tags=%s, orderBy=%s, desc=%s}", name, surname,
-                             tags, orderBy, desc);
+        return String.format("SearchCriteria{name='%s', surname='%s', tags=%s, orderBy=%s}", name, surname,
+                tags, orderBy);
     }
 }
