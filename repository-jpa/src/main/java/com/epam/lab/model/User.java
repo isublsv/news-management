eec827ad6ea2dd9605a75e23c93540dd3a0c585f
Dmitry G <isublsv@gmail.com>
2020-03-26 16:43:43 +0300

    Add role entity support for user

--- repository-jpa/src/main/java/com/epam/lab/model/User.java
+++ repository-jpa/src/main/java/com/epam/lab/model/User.java
@@ -1,10 +1,18 @@
 package com.epam.lab.model;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "user", schema = "news", uniqueConstraints = @UniqueConstraint(columnNames = "login"))
@@ -21,6 +29,16 @@ public class User extends AbstractEntity implements Pageable {
 
     @Column(name = "password", length = 30, nullable = false)
     private String password;
+    
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "roles",
+            schema = "news",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Column(name = "role_name", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
         super();
@@ -30,53 +48,66 @@ public class User extends AbstractEntity implements Pageable {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
+    public void setName(final String nameValue) {
+        name = nameValue;
     }
 
     public String getSurname() {
         return surname;
     }
 
-    public void setSurname(final String surname) {
-        this.surname = surname;
+    public void setSurname(final String surnameValue) {
+        surname = surnameValue;
     }
 
     public String getLogin() {
         return login;
     }
 
-    public void setLogin(final String login) {
-        this.login = login;
+    public void setLogin(final String loginValue) {
+        login = loginValue;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(final String password) {
-        this.password = password;
+    public void setPassword(final String passwordValue) {
+        password = passwordValue;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(final Set<Role> rolesValue) {
+        roles = rolesValue;
     }
 
     @Override
     public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
         return name.equals(user.name)
                && surname.equals(user.surname)
                && login.equals(user.login)
-               && password.equals(user.password);
+               && password.equals(user.password)
+               && roles.equals(user.roles);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, surname, login, password);
+        return Objects.hash(name, surname, login, password, roles);
     }
 
     @Override
     public String toString() {
-        return String.format("User{id=%d, name='%s', surname='%s', login='%s', password='%s'}",
-                             getId(), name, surname, login, password);
+        return String.format("User{id=%d, name='%s', surname='%s', login='%s', password='%s', roles='%s'}",
+                             getId(), name, surname, login, password, roles);
     }
 }
--- service-jpa/src/main/java/com/epam/lab/dto/UserDto.java
+++ service-jpa/src/main/java/com/epam/lab/dto/UserDto.java
@@ -1,23 +1,26 @@
 package com.epam.lab.dto;
 
 import com.epam.lab.model.Pageable;
+import com.epam.lab.model.Role;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class UserDto extends AbstractDto implements Pageable {
 
     @NotBlank(message = "Provided user name must not be blank")
-    @Length(min = 2, max = 30, message = "The user name length must be between 2 and 30 characters.")
+    @Length(min = 2, max = 20, message = "The user name length must be between 2 and 20 characters.")
     @Pattern(regexp = "^[A-ZА-Я]+",
             flags = Pattern.Flag.CASE_INSENSITIVE,
             message = "Provided user name is not valid")
     private String name;
 
     @NotBlank(message = "Provided user surname must not be blank")
-    @Length(min = 2, max = 30, message = "The user surname length must be between 2 and 30 characters.")
+    @Length(min = 2, max = 20, message = "The user surname length must be between 2 and 20 characters.")
     @Pattern(regexp = "^[A-ZА-Я\\-]+",
             flags = Pattern.Flag.CASE_INSENSITIVE,
             message = "Provided user surname is not valid")
@@ -36,6 +39,8 @@ public class UserDto extends AbstractDto implements Pageable {
             flags = Pattern.Flag.CASE_INSENSITIVE,
             message = "Provided user password is not valid")
     private String password;
+
+    private Set<Role> roles = new HashSet<>();
 
     public UserDto() {
         super();
@@ -73,28 +78,38 @@ public class UserDto extends AbstractDto implements Pageable {
         password = passwordValue;
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(final Set<Role> rolesValue) {
+        roles = rolesValue;
+    }
+
     @Override
-    public boolean equals(final Object oValue) {
-        if (this == oValue) {
+    public boolean equals(final Object o) {
+        if (this == o) {
             return true;
         }
-        if (oValue == null || getClass() != oValue.getClass()) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        UserDto userDto = (UserDto) oValue;
+        UserDto userDto = (UserDto) o;
         return name.equals(userDto.name)
                && surname.equals(userDto.surname)
                && login.equals(userDto.login)
-               && password.equals(userDto.password);
+               && password.equals(userDto.password)
+               && roles.equals(userDto.roles);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, surname, login, password);
+        return Objects.hash(name, surname, login, password, roles);
     }
 
     @Override
     public String toString() {
-        return String.format("UserDto{id=%d, name='%s', surname='%s', login='%s', password='%s'}", getId(), name, surname, login, password);
+        return String.format("UserDto{id=%d, name='%s', surname='%s', login='%s', password='%s', roles='%s'}",
+                             getId(), name, surname, login, password, roles);
     }
 }
