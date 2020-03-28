6ec8b5c4019bd0ea865f776fa35e79a73c52eb00
Dmitry G <isublsv@gmail.com>
2020-03-28 13:35:31 +0300

    Add security configuration

---
+++ controller/src/main/java/com/epam/lab/configuration/WebSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.epam.lab.configuration;
+
+import com.epam.lab.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        // securedEnabled = true,
+        // jsr250Enabled = true,
+        prePostEnabled = true)
+@PropertySource("classpath:application.properties")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder)
+            throws Exception {
+        authenticationManagerBuilder.userDetailsService(userService)
+                                    .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+            .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .authorizeRequests()
+                .antMatchers("/auth/signup", "/author/findAll").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+}
---
+++ controller/src/main/java/com/epam/lab/controller/JwtResponse.java
@@ -0,0 +1,85 @@
+package com.epam.lab.controller;
+
+import java.util.List;
+
+public class JwtResponse {
+    private String token;
+    private String type;
+    private Long id;
+    private String name;
+    private String surname;
+    private String login;
+    private List<String> roles;
+
+    public JwtResponse(
+            final String tokenValue,
+            final Long idValue,
+            final String nameValue,
+            final String surnameValue,
+            final String loginValue,
+            final List<String> rolesValue) {
+        token = tokenValue;
+        type = "Bearer";
+        id = idValue;
+        name = nameValue;
+        surname = surnameValue;
+        login = loginValue;
+        roles = rolesValue;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(final String tokenValue) {
+        token = tokenValue;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(final String typeValue) {
+        type = typeValue;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long idValue) {
+        id = idValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String nameValue) {
+        name = nameValue;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(final String surnameValue) {
+        surname = surnameValue;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(final String loginValue) {
+        login = loginValue;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(final List<String> rolesValue) {
+        roles = rolesValue;
+    }
+}
